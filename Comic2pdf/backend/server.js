757c27aed22857b21a566d84b7e40bf332f64686
SHREE167 <148609671+SHREE167@users.noreply.github.com>
2025-05-05 16:55:25 +0530

    Update server.js

--- Comic2pdf/backend/server.js
+++ Comic2pdf/backend/server.js
@@ -4,6 +4,27 @@ const cheerio = require("cheerio");
 const sharp = require("sharp");
 const PDFDocument = require("pdfkit");
 const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// API routes here
+// app.use("/api", apiRoutes);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 const app = express();
 app.use(cors());
